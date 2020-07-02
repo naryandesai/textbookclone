8bc2f48fc33638310d49c740f9cccb1e1f641c4d
sarkapoor <sarkapoor@utexas.edu>
2020-07-01 19:35:33 -0500

    Login changes

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 export default {
   region: "us-east-1",
-  IdentityPoolId: "us-east-1_jdETcc6Zw",
-  UserPoolId: "us-east-1:690430404508",
-  ClientId: "673k7kbuq3pnmomu14lk4e78ns"
+  IdentityPoolId: "us-east-1_qzgXkEuBO",
+  UserPoolId: "us-east-1:083124225867",
+  ClientId: "2a5qr42eeu4h55mk7ul6bphn2b"
 };
